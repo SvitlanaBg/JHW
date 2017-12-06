25d6235bc1bcdaefd73596af2f667ef06302256f
SvitlanaBg <31420241+SvitlanaBg@users.noreply.github.com>
2017-12-06 22:28:05 +0200

    l8

--- src/lesson8/Task2Person.java
+++ src/lesson8/Task2Person.java
@@ -11,7 +11,6 @@ public class Task2Person {
     public static void main(String[] args) {
         Person person1 = new Person();
         Person person2 = new Person("Ivanchenko Ivan", 15);
-
         System.out.println(person1.getFullName() + " " + person1.getAge());
     }
 }
