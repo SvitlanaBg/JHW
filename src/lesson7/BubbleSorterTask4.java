b32fc20b41023fa4a8a504a04f0e45517238efa3
SvitlanaBg <31420241+SvitlanaBg@users.noreply.github.com>
2017-12-07 16:00:58 +0200

    l8

--- src/lesson7/BubbleSorterTask4.java
+++ src/lesson7/BubbleSorterTask4.java
@@ -11,29 +11,31 @@ import java.util.Arrays;
 
 public class BubbleSorterTask4 {
     public static void main(String[] args) {
-        int a = 3;
         int[][] data = {
                 {0, 2, 5, 3, 4, 1},
-                {5, 6, 8, 2, 1, 7},
+                {5, 6, 8, 2, 1, 0},
         };
         for (int[] arr : data) {
             System.out.println(Arrays.toString(arr));
-            sort(arr, a);
+            sort(arr);
             System.out.println(Arrays.toString(arr));
+            System.out.println();
         }
     }
 
-    public static void sort(int[] array, int a) {
+    public static void sort(int[] array) {
         int i, j;
         for (i = 0; i < array.length; i++) {
+            boolean flag = false;
             for (j = 0; j < array.length - 1; j++) {
                 if (array[j + 1] > array[j]) {
                     int tmp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = tmp;
+                    flag = true;
                 }
             }
-            if (a == i) {
+            if (!flag) {
                 break;
             }
         }
